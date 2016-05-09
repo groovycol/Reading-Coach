d454a631a4eee9b6f608e17d673888dea8334e08
Colleen <groovycol@gmail.com>
2016-05-09 16:03:32 -0700

    updated /login route to check the database and redirect user to the login page with messages about mismatched user or password information. updated the /register route to add new users to the dbase if the user_id doesn't already exist.

--- server.py
+++ server.py
@@ -1,10 +1,16 @@
 
 from flask import Flask, render_template, request, flash, redirect, session
-# from model import connect_to_db,
+from model import connect_to_db, db, Coach, Reader, Teacher, NameTitle, ReadingLogs
+
+from jinja2 import StrictUndefined
+
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.secret_key = "secret"
 
+# Force jinja to raise an error
+app.jinja_env.undefined = StrictUndefined
 
 @app.route("/")
 def index():
@@ -25,9 +31,22 @@ def login_process():
     user_id = request.form["user_id"]
     password = request.form["password"]
 
-    session["user_id"] = user_id
+    #make sure this user_id and password match in the database
 
-    return redirect("/record")
+    user = Coach.query.filter_by(phone=user_id).first()
+
+    if not user:
+        flash("No such user")
+        return redirect("/login")
+
+    if user.password != password:
+        flash("Incorrect password")
+        return redirect("/login")
+    else:
+        #add user_id to the session
+        session["user_id"] = user_id
+
+        return redirect("/record")
 
 
 @app.route("/logout")
@@ -37,6 +56,45 @@ def logout():
     flash("You have logged out.")
 
     return redirect("/login")
+
+
+#Manage new user registrations
+@app.route('/register')
+def register():
+    """Add a new user to the database"""
+
+    return render_template("register.html")
+
+
+@app.route('/register_process', methods=['POST'])
+def register_process():
+    """Process registration."""
+
+    user_id = request.form["user_id"]
+    password = request.form["password"]
+    email = request.form["email"]
+
+    #make sure this user_id isn't already in use
+    user = Coach.query.filter_by(phone=user_id).first()
+
+    if not user:
+        #Add new user_id to the database
+        new_coach = Coach(phone=user_id,
+                        password=password,
+                        email=email)
+        db.session.add(new_coach)
+        db.session.commit()
+
+        #Give the user a confirmation message about being registered.
+        flash(user_id + " is now registered to receive text message reminders")
+        #Add the new user_id to the session to keep user logged in.
+        session["user_id"] = user_id
+    else:
+        #already in the dbase, redirect to login page
+        flash(user_id + " is already registered")
+        return redirect("/login")
+
+    return render_template("new-user-info.html")
 
 
 #Routes to manage user input and displaying user data
@@ -61,6 +119,12 @@ def show_dashboard():
 
 
 if __name__ == "__main__":
+    # turn this off for demos
     app.debug = True
-    # connect_to_db(app)
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
     app.run()
